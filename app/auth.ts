5bc68ea175958cfc557f84e2b2ef2d187283a382
ootoro29 <gobou256@gmail.com>
2024-06-17 17:26:08 +0900

    prisma変更

--- app/auth.ts
+++ app/auth.ts
@@ -1,5 +1,11 @@
+import { pool } from "./db";
 import { PrismaClient } from "@prisma/client";
+//import { PrismaPg } from "@prisma/adapter-pg";
+
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+//const adapter = new PrismaPg(pool);
+//export const prisma = new PrismaClient({adapter});
